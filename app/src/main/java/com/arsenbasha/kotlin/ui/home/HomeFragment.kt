f5b70fb38df6906bec6e7d911d45b9fb7e158c23
Arsenbasha <leandroparedes0721@gmail.com>
2021-03-27 18:34:08 +0100

    quitando cosas

--- app/src/main/java/com/arsenbasha/kotlin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/arsenbasha/kotlin/ui/home/HomeFragment.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.arsenbasha.kotlin.R
@@ -35,20 +33,9 @@ class HomeFragment : Fragment() {
         val video =homeBinding.video
         video.setVideoURI(Uri.parse(path))
         val play=homeBinding.play
-        val pause=homeBinding.pausa
-
-
-        play.setOnClickListener {
-            video.start()
-            play.visibility=View.INVISIBLE
-            pause.visibility = View.VISIBLE
-        }
-        pause.setOnClickListener {
-            video.pause()
-            pause.visibility=View.INVISIBLE
-            play.visibility = View.VISIBLE
-
-        }
+        val pause=homeBinding.pause
+        play.setOnClickListener { video.start()}
+        pause.setOnClickListener { video.pause()}
     }
 
 }
